16c089ff927aba9c906769860b35aa7799227d20
jose matheus <joseribeiroflora@gmail.com>
2022-04-11 19:16:42 -0300

    iniciando o git

---
+++ aluno.java
@@ -0,0 +1,16 @@
+package pacote;
+
+public class aluno {
+	private String nome;
+	private String matricula;
+	private String endereço;
+	private String cpf;
+	
+	public aluno(String nome,String matricula,String endereço,String cpf){
+		this.nome = nome;
+		this.matricula =matricula;
+		this.endereço = endereço;
+		this.cpf = cpf;
+	}
+	
+}
