2350e37c42b92a2bf21053679467a3a241817058
jose matheus <joseribeiroflora@gmail.com>
2022-04-11 20:34:32 -0300

    hello world

---
+++ hello-world/src/pacote/main.java
@@ -0,0 +1,12 @@
+package pacote;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="ola";
+		String s2="mundo";
+		System.out.println(s1 + s2);
+	}
+
+}
